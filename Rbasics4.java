b4730aaea0e97fd3e43db32bc0f1ab9bb4037d6b
Arushi singh <85683967+Arushisingh23@users.noreply.github.com>
2023-03-01 23:45:18 +0530

    Create Rbasics4.java

---
+++ Rbasics4.java
@@ -0,0 +1,18 @@
+
+//factorial of number n 
+package com.recursion;
+public class Rbasics4 {
+
+    static int factorial(int n) {
+        // base case
+        if (n == 0) {
+            return 1;
+        }
+        // calling recursive function
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Factorial of the number 5 is: " + factorial(5));
+    }
+}
